0f1cd916825f1f5163fa67951679078a8b4468ec
Alexander Zhelyazov <azhelyazov@gmail.com>
2019-02-20 23:24:20 +0000

    refactor resolvers with async/await

--- src/resolvers.js
+++ src/resolvers.js
@@ -4,98 +4,113 @@ const User = require('./models/user');
 const Entry = require('./models/entry');
 const bcrypt = require('bcryptjs');
 
+const populateUser = async userId => {
+    try {
+        const user = await User.findById(userId);
+        return {
+            ...user._doc,
+            _id: user.id,
+            entries: populateEntries.bind(this, user._doc.entries),
+        };
+    } catch (err) {
+        throw err;
+    }
+};
+
+const populateEntries = async ids => {
+    try {
+        const entries = await Entry.find({ _id: { $in: ids } });
+        return entries.map(entry => {
+            return {
+                ...entry._doc,
+                _id: entry.id,
+                addedBy: populateUser.bind(this, entry.addedBy),
+            };
+        });
+    } catch (err) {
+        throw err;
+    }
+};
+
 module.exports = {
     Query: {
         user: (root, { id }) => users[id],
-        users: () => {
-            return User.find()
-                .then(result =>
-                    result.map(user => {
-                        return {
-                            ...user._doc,
-                            _id: user.id,
-                        };
-                    }),
-                )
-                .catch(err => {
-                    throw err;
+        users: async () => {
+            try {
+                const result = await User.find();
+                return result.map(user => {
+                    return {
+                        ...user._doc,
+                        _id: user.id,
+                        entries: populateEntries.bind(this, user._doc.entries),
+                    };
                 });
+            } catch (err) {
+                throw err;
+            }
         },
-        entries: () => {
-            return Entry.find()
-                .populate('addedBy')
-                .then(result =>
-                    result.map(entry => {
-                        return {
-                            ...entry._doc,
-                            _id: entry.id,
-                        };
-                    }),
-                )
-                .catch(err => {
-                    throw err;
+        entries: async () => {
+            try {
+                const result = await Entry.find();
+                return result.map(entry => {
+                    return {
+                        ...entry._doc,
+                        _id: entry.id,
+                        addedBy: populateUser.bind(this, entry._doc.addedBy),
+                    };
                 });
+            } catch (err) {
+                throw err;
+            }
         },
     },
     Mutation: {
-        createUser: (root, { UserInput: { email, password, name } }) => {
-            return User.findOne({ email })
-                .then(user => {
-                    if (user) {
-                        throw Error('User already exists.');
-                    }
-                    return bcrypt.hash(password, 12);
-                })
-                .then(hashedPassword => {
-                    const user = new User({
-                        email,
-                        password: hashedPassword,
-                        name,
-                    });
-                    return user.save();
-                })
-                .then(result => {
-                    return {
-                        ...result._doc,
-                        _id: result.id,
-                        // we don't want to be able to retrieve passwords
-                        password: null,
-                    };
-                })
-                .catch(err => {
-                    throw err;
+        createUser: async (root, { UserInput: { email, password, name } }) => {
+            try {
+                if (await User.findOne({ email })) {
+                    throw Error('User already exists.');
+                }
+                const user = new User({
+                    email,
+                    password: await bcrypt.hash(password, 12),
+                    name,
                 });
+
+                const result = await user.save();
+
+                return {
+                    ...result._doc,
+                    _id: result.id,
+                    // we don't want to be able to retrieve passwords
+                    password: null,
+                };
+            } catch (err) {
+                throw err;
+            }
         },
-        createEntry: (root, { EntryInput: { mood, note } }) => {
+        createEntry: async (root, { EntryInput: { mood, note } }) => {
             let newEntry;
             const entry = new Entry({
                 mood,
                 note,
                 addedBy: '5c6dcf5bdd89210a6d1fa4c8',
             });
-
-            return entry
-                .save()
-                .then(result => {
-                    newEntry = {
-                        ...result._doc,
-                        _id: result.id,
-                    };
-                    return User.findById('5c6dcf5bdd89210a6d1fa4c8');
-                })
-                .then(user => {
-                    if (!user) {
-                        throw Error('User does not exist');
-                    }
-                    user.entries.push(entry);
-                    return user.save();
-                })
-                .then(result => {
-                    return newEntry;
-                })
-                .catch(err => {
-                    throw err;
-                });
+            try {
+                const result = await entry.save();
+                newEntry = {
+                    ...result._doc,
+                    _id: result.id,
+                };
+                const user = await User.findById('5c6dcf5bdd89210a6d1fa4c8');
+                if (!user) {
+                    throw Error('User does not exist.');
+                }
+                user.entries.push(entry);
+                await user.save();
+                return newEntry;
+            } catch (err) {
+                throw err;
+            }
         },
     },
     Date: new GraphQLScalarType({
